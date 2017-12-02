8b2f2ce3ba12493867c307d1353df582e1d6c154
xfya <349536842@qq.com>
2017-12-03 07:25:57 +0800

    music-list 完成

--- README.md
+++ README.md
@@ -146,3 +146,6 @@ ES5实现：
 probeType: 1：滚动的时候会派发scroll事件，会截流。2：滚动的时候实时派发scroll事件，不会截流。 3：除了实时派发scroll事件，在swipe的情况下仍然能实时派发scroll事件
 
 
+###  后退  this.$router.back()
+
+
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,11 +1,21 @@
 <template>
 
     <div class="music-list">
-            <div class="back" >
+            <div class="back" @click="back">
                 <i class="icon-back"></i>
             </div>
             <h1 class="title" v-html="title"></h1>
-            <div class="bg-image"  :style="bgStyle"   ref="bgImage"></div>
+            <div class="bg-image"  :style="bgStyle"   ref="bgImage">
+
+                      <div class="play-wrapper">
+                        <div ref="playBtn" v-show="songs.length>0" class="play" @click="random">
+                        <i class="icon-play"></i>
+                        <span class="text">随机播放全部</span>
+                        </div>
+                    </div>
+
+
+            </div>
            
                <!--滑动辅助层-->
              <div class="bg-layer" ref="layer"></div>
@@ -22,6 +32,10 @@
                     >
                     </song-list>   
                 </div>
+
+                      <div v-show="!songs.length" class="loading-container">
+                            <loading></loading>
+                      </div>
             </scroll>
     </div>
 
@@ -30,6 +44,7 @@
 <script>
     import SongList from 'base/song-list/song-list'
     import Scroll from '@/base/scroll/scroll'
+      import Loading from 'base/loading'
     import {prefixStyle} from 'common/js/dom'
     const transform = prefixStyle('transform')
     const RESERVED_HEIGHT = 40
@@ -53,6 +68,13 @@
         methods:{
             scroll(pos){
                 this.scrollY = pos.y
+            },
+            back(){
+                 this.$router.back()
+                //        window.history.back(-1); 
+            },
+            random(){
+
             }
         },
         props:{
@@ -80,13 +102,33 @@
         },
         components:{
             Scroll,
-            SongList
+            SongList,
+            Loading
         },
         watch:{
              scrollY(newVal) {
+                let zIndex = 0
+                let scale = 1
                 let translateY = Math.max(this.minTransalteY, newVal)
-               
+                const percent = Math.abs(newVal / this.imageHeight)  
                 this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+                if (newVal > 0) {
+                    scale = 1 + percent
+                    zIndex = 10
+                }
+                 if (newVal < this.minTransalteY) {
+                    zIndex = 10
+                    this.$refs.bgImage.style.paddingTop = 0
+                    this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px` 
+                    this.$refs.playBtn.style.display = 'none'
+                 }else{
+                    this.$refs.bgImage.style.paddingTop = '70%'
+                    this.$refs.bgImage.style.height = 0
+                    this.$refs.playBtn.style.display = ''
+                 }
+
+                this.$refs.bgImage.style.zIndex = zIndex
+                this.$refs.bgImage.style[transform] = `scale(${scale})`
             }
         }
     }
